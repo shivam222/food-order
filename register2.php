b223f0631e262ee30fa7c872adea006c448dd2ad
shivam222 <shivamb61@gmail.com>
2017-01-21 19:01:21 +0530

    log in

---
+++ register2.php
@@ -0,0 +1,34 @@
+<?php
+
+$servername = "localhost:3307";
+$username = "root";
+$password = "";
+$db_name="users";
+$connection=mysql_connect($servername,$username,$password);
+
+$database=mysql_select_db($db_name,$connection);
+	
+	//$_POST = json_decode(file_get_contents('php://input'), true);
+	    $name=$_POST['name'];
+	    $pass=$_POST['pass'];
+		$hashed=md5($pass);
+		
+		$query="SELECT password,uname FROM food WHERE email='$name'";
+		$run=mysql_query($query);
+		$ansi=mysql_fetch_assoc($run);
+		if($hashed==$ansi['password']){
+		   echo $ansi['uname'];
+		}
+	    else{
+		
+		echo false;
+		}
+		
+?>
+		
+	
+	
+	
+	
+
+
--- rough.js
+++ rough.js
@@ -1,4 +1,4 @@
-var app=angular.module('apple',['ngRoute']);
+var app=angular.module('apple',['ngRoute','ngCookies']);
 
 app.config(function($routeProvider){
 	$routeProvider
@@ -6,10 +6,25 @@ app.config(function($routeProvider){
 	controller  : 'love'});
 });
 
-app.controller('love',function($scope){
+app.controller('love',function($scope,$http,$rootScope,$cookies){
+	
+	
+	if($cookies.get('remail2')=='yep')
+	{
+	     
+        $scope.log3=false;		
+        $scope.sign=true;
+        $scope.sign5=true;	
+        $scope.log2=true;		
+		$scope.sir=$cookies.get('remail');
+	}
+	else{
 	$scope.log2=true;
+	$scope.log3=true;
+	}
 	$scope.change=function(){
 		$scope.sign=true;
+		$scope.sign5=false;
 		$scope.log2=false;
 		$scope.log={"color":"green"};
 		$scope.sign2={"color":"#666"};
@@ -19,6 +34,55 @@ app.controller('love',function($scope){
 		$scope.log2=true;
 		$scope.log={"color":"#666"};
 		$scope.sign2={"color":"green"};
+	}
+	//ajax request for login
+	
+	var expireDate = new Date();
+    expireDate.setDate(expireDate.getDate() + 2);
+	$scope.submit=function(){
+		var emailer=$scope.email2;
+		
+			$http({
+    method: 'post',
+    url: 'register2.php',
+	data : $.param({
+                name: $scope.email2,
+				pass: $scope.password2
+            }),
+    headers: {
+        'Content-Type': "application/x-www-form-urlencoded; "    //charset=utf-8
+              }
+                                    
+	 })
+	.success(function(data) {	
+ 	    $rootScope.loggged=data;
+		if($rootScope.loggged!=false){
+			$scope.log2=true;
+			$scope.sir=$rootScope.loggged;
+			$scope.log3=false;
+			$scope.msg=" " ;
+			$scope.sign5=true;
+			$cookies.put('remail2','yep', {'expires': expireDate});
+			$cookies.put('remail',emailer , {'expires': expireDate});
+			
+		}
+		else{
+			$scope.msg="something wrong";
+		}
+		
+             
+
+	 });
+	}
+	$scope.logout=function(){
+		$cookies.remove('remail2');
+		$cookies.remove('remail');
+		$scope.sign5=false;
+		$scope.log2=false;
+		$scope.log3=true;
+		$scope.sir="";
+		$scope.msg="";
+		
 	}
 	
 });
