62f1de1ad0220d943f9b15cb4d716b444036da7a
shivam222 <shivamb61@gmail.com>
2017-02-05 01:30:58 +0530

    order thali

--- thali.php
+++ thali.php
@@ -9,12 +9,20 @@ $database=mysql_select_db($db_name,$connection);
 	
 	
 	    
-		$query1="SELECT item FROM thali";
+		$query1="SELECT * FROM thali";
 		$run1=mysql_query($query1);
-		$ansi=mysql_fetch_assoc($run1);
-		if($run1)
-		{
-	    echo $ansi['item'];
-		}
+	    //echo $run1[0];
+		$data=array();
+		$i=0;
+		while($ansi=mysql_fetch_assoc($run1)){
+		   $data[$i]= $ansi['item'];
+		   $i++;
+		   }
+		   echo json_encode($data);
+		//header('Content-Type: application/json');
+		//if($run1)
+		//{
+	    //echo $ansi['item'];
+		//}
 		
 ?>
