b518713f20c8f7c324f66c999ca66976296a89f8
shivam222 <shivamb61@gmail.com>
2017-02-19 13:27:05 +0530

    thali summary

--- rough.js
+++ rough.js
@@ -11,7 +11,9 @@ app.config(function($routeProvider){
 	.when('/help',{templateUrl:'help.html',
 	controller  : 'help'})
 	.when('/order',{templateUrl:'order.html',
-	controller : 'order'});
+	controller : 'order'})
+	.when('/summary',{templateUrl:'summary.html',
+	controller : 'summary'});
 });
 
 app.controller('love',function($scope,$http,$rootScope,$cookies){
@@ -154,25 +156,25 @@ app.controller('help',function($scope,$location,$anchorScroll){
 	}
 });
 
-app.controller('order',function($scope,$http){
+app.controller('order',function($scope,$http,$rootScope){
 	
-	$scope.qty=0;
+	$rootScope.qty=0;
 	$http({
   method: 'GET',
   url: 'thali.php'
 }).success(function(response) {
     $scope.items=response;
 	var l=$scope.items.length;
-	$scope.cost=$scope.items[l-1];
+	$rootScope.cost=$scope.items[l-1];
 	$scope.items.pop();
 	  });
 	  
 	 $scope.thaliqty=function(para){
 		 if(para=="add"){
-			 $scope.qty=$scope.qty+1;
+			 $rootScope.qty=$rootScope.qty+1;
 		 }
-		 else if(para=="sub" && $scope.qty>0){
-			 $scope.qty--;
+		 else if(para=="sub" && $rootScope.qty>0){
+			 $rootScope.qty--;
 		 }
 	 }
 	 
@@ -194,20 +196,25 @@ $http({
    for(var v=0;v<response2.length;v++){
 	   quantity[v]=0;
    }
-   $scope.special=quantity;
+   $rootScope.special=quantity;
 });
 $scope.degrade=function(now){
-	if($scope.special[now]>0)
-	   $scope.special[now]--;
+	if($rootScope.special[now]>0)
+	   $rootScope.special[now]--;
 }
 $scope.upgrade=function(now){
-        $scope.special[now]++;	
+        $rootScope.special[now]++;	
 }
-$scope.summary=function(){
+/*$scope.summary=function(){
 	console.log($scope.special);
 	console.log($scope.qty);
-}
+}*/
 
+});
+app.controller('summary',function($scope,$rootScope){
+	
+	$scope.thaliq=$rootScope.qty;
+	$scope.thalip=$rootScope.cost*$rootScope.qty;
 });
 
 
